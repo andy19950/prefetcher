65477cda8e1ed03871fa11dcc233de5a2ae1143e
Andy <andy19950@gmail.com>
2016-10-13 15:49:00 +0800

    calculate the runtime by unsing time_test

---
+++ benchmark_clock_gettime.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "impl.h"
+
+#define CLOCK_ID CLOCK_MONOTONIC_RAW
+#define ONE_SEC 1000000000.0
+#define TEST_W 4096
+#define TEST_H 4096
+
+
+int main(int argc, char const *argv[])
+{
+    struct timespec start = {0, 0};
+    struct timespec end = {0, 0};
+
+    int *src  = (int *) malloc(sizeof(int) * TEST_W * TEST_H);
+    int *out = (int *) malloc(sizeof(int) * TEST_W * TEST_H);
+
+    srand(time(NULL));
+    for (int y = 0; y < TEST_H; y++)
+        for (int x = 0; x < TEST_W; x++)
+            *(src + y * TEST_W + x) = rand();
+
+    // Naive
+    clock_gettime(CLOCK_ID, &start);
+    naive_transpose(src, out, TEST_W, TEST_H);
+    clock_gettime(CLOCK_ID, &end);
+    printf("%.4f ", (double) (end.tv_sec - start.tv_sec) +
+           (end.tv_nsec - start.tv_nsec)/ONE_SEC);
+
+
+    // SSE
+    clock_gettime(CLOCK_ID, &start);
+    sse_transpose(src, out, TEST_W, TEST_H);
+    clock_gettime(CLOCK_ID, &end);
+    printf("%.4f ", (double) (end.tv_sec - start.tv_sec) +
+           (end.tv_nsec - start.tv_nsec)/ONE_SEC);
+
+
+    // SSE prefetch
+    clock_gettime(CLOCK_ID, &start);
+    sse_prefetch_transpose(src, out, TEST_W, TEST_H);
+    clock_gettime(CLOCK_ID, &end);
+    printf("%.4f\n", (double) (end.tv_sec - start.tv_sec) +
+           (end.tv_nsec - start.tv_nsec)/ONE_SEC);
+
+
+    // SSE prefetch AVX
+    //clock_gettime(CLOCK_ID, &start);
+    //clock_gettime(CLOCK_ID, &end);
+    //printf("%.4f ", (double) (end.tv_sec - start.tv_sec) +
+    //     (end.tv_nsec - start.tv_nsec)/ONE_SEC);
+
+
+    return 0;
+}
